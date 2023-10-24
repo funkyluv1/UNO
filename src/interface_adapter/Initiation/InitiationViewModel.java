57c4ce3e19f4ed3742956ced8fc1b24be5bcb302
Daniel Niu <danielfqn@gmail.com>
2023-10-23 20:53:06 -0400

    view and viewmodel

--- src/interface_adapter/Initiation/InitiationViewModel.java
+++ src/interface_adapter/Initiation/InitiationViewModel.java
@@ -4,24 +4,23 @@ import interface_adapter.Initiation.ViewModel;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-public class initiationViewModel {
+public class InitiationViewModel extends ViewModel{
     public static final String TITLE_LABEL = "Game View";
-    public static final String INITIATION_BUTTON_LABEL = "Sign up";
-    public static final String CANCEL_BUTTON_LABEL = "Cancel";
+    public static final String INITIATION_BUTTON_LABEL = "Initiation";
 
-    private SignupState state = new SignupState();
+    private InitiationState state = new InitiationState();
 
-    public SignupViewModel() {
-        super("sign up");
+    public InitiationViewModel() {
+        super("initiation");
     }
 
-    public void setState(SignupState state) {
+    public void setState(InitiationState state) {
         this.state = state;
     }
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
-    // This is what the Signup Presenter will call to let the ViewModel know
+    // This is what the Initiation Presenter will call to let the ViewModel know
     // to alert the View
     public void firePropertyChanged() {
         support.firePropertyChange("state", null, this.state);
@@ -31,7 +30,7 @@ public class initiationViewModel {
         support.addPropertyChangeListener(listener);
     }
 
-    public SignupState getState() {
+    public InitiationState getState() {
         return state;
     }
 }
