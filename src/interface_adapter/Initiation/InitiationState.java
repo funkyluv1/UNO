ec6022b0abe3dbe90a50e669e240bf2e574bfca9
Daniel Niu <danielfqn@gmail.com>
2023-11-05 15:38:57 -0500

    new commit

--- src/interface_adapter/Initiation/InitiationState.java
+++ src/interface_adapter/Initiation/InitiationState.java
@@ -1,17 +1,21 @@
 package interface_adapter.Initiation;
 
 public class InitiationState {
-    private Game game;
+//    private Game game;
     private String initiationError = null;
 
     public InitiationState(InitiationState copy){
-        this.g = game;
-        this.initiationError = initiationError;
+//        game = copy.game;
+        initiationError = copy.initiationError;
     }
 
-    public void set_game(Game game){this.g = game;}
+    public InitiationState(){}
 
     public Game get_game(){return game;}
+
+    public Game get_initiationError(){return initiationError;}
+
+    public void set_game(Game game){this.g = game;}
 
     public void set_initiationError(String initiationError){this.initiationError = initiationError;}
 
