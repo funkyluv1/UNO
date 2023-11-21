3a1b9fec3abff152fbfb954cbad352ce4f842158
Daniel Niu <danielfqn@gmail.com>
2023-11-20 19:50:32 -0500

    newest clean version

--- src/view/InitializedView.java
+++ src/view/InitializedView.java
@@ -11,56 +11,12 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
-//public class InitializedView extends JPanel implements ActionListener, PropertyChangeListener {
-//    public final String viewName = "Initialized";
-//    private final InitializedViewModel initializedViewModel;
-//
-//    JLabel playername;
-//
-//    public InitializedView(InitializedViewModel initializedViewModel) {
-//        this.initializedViewModel = initializedViewModel;
-//        this.initializedViewModel.addPropertyChangeListener(this);
-//
-//        JLabel title = new JLabel("Initialized Screen");
-//        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-//
-//        JLabel usernameInfo = new JLabel("Currently players: ");
-//        playername = new JLabel();
-//
-//        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//
-//        this.add(title);
-//        this.add(usernameInfo);
-//        this.add(playername);
-//    }
-//
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        System.out.println("Click " + e.getActionCommand());
-//    }
-//
-//    @Override
-//    public void propertyChange(PropertyChangeEvent evt) {
-//        InitializedState state = (InitializedState) evt.getNewValue();
-//        // we don't know how many players there are, so we can't create player1, player2 ...
-//        // this is the best we can do, display all players in one string
-//        String output = "";
-//        ArrayList<String> players = state.get_players();
-//        for (int i = 0; i < players.size(); i++)
-//            output += (players.get(i) + "\n");
-//        playername.setText(output);
-//    }
-//}
 public class InitializedView extends JPanel implements ActionListener, PropertyChangeListener{
 
     public final String viewName;
     private final InitializedViewModel initializedViewModel;
 
     ArrayList<JLabel> usernames = new ArrayList<>();
-    JLabel usernameLabel1;
-    JLabel usernameLabel2;
-    JLabel usernameLabel3;
-    JLabel usernameLabel4;
 
     public InitializedView(InitializedViewModel initializedViewModel) {
         JLabel title = new JLabel("Initialized Screen");
