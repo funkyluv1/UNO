07215dd721f7f2313bc4215f94831002c41717e0
bluenut15 <bluenut15@gmail.com>
2023-11-20 20:11:40 -0500

    some accidentally deleted presenters restored

---
+++ src/interface_adapter/SelectCard/SelectCardPresenter.java
@@ -0,0 +1,11 @@
+package interface_adapter.SelectCard;
+
+import use_case.SelectCard.SelectCardOutputData;
+import use_case.SelectCard.SelectCardOutputDataBoundary;
+
+public class SelectCardPresenter implements SelectCardOutputDataBoundary {
+    @Override
+    public void prepareSelectCardView(SelectCardOutputData selectCardOutputData) {
+
+    }
+}
---
+++ src/interface_adapter/Undo/UndoPresenter.java
@@ -0,0 +1,10 @@
+package interface_adapter.Undo;
+
+import use_case.Undo.UndoOutputDataBoundary;
+
+public class UndoPresenter implements UndoOutputDataBoundary {
+    @Override
+    public void prepareUndoView() {
+
+    }
+}
