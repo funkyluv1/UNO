cf1a8dd5b011aa6a8124f42b9b1cc72091e1893a
Siwei Zhan <siwei.zhan@mail.utoronto.ca>
2023-11-28 00:46:40 -0500

    Test updated

--- src/view/InitializedView.java
+++ src/view/InitializedView.java
@@ -139,11 +139,9 @@ public class InitializedView extends JPanel implements ActionListener, PropertyC
             usernames.get(i).setText(players.get(i));
         }
 
-        for (JButton cardName : cardNames) {
             for (int i = 0; i < 3; i++) {
                 String name = initializedViewModel.getState().get_Number_Cards().get(i).getString();
-                cardName.setText(name);
-            }
+                cardNames.get(i).setText(name);
         }
     }
 
