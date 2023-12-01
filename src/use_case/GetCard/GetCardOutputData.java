d00133a4ab3af38902526bba8c8137017d09b163
asdf <davidpark529kr@gmail.com>
2023-12-01 10:15:16 -0500

    added inputdata

--- src/use_case/GetCard/GetCardOutputData.java
+++ src/use_case/GetCard/GetCardOutputData.java
@@ -1,2 +1,16 @@
-package use_case.GetCard;public class GetCardOutputData {
+package use_case.GetCard;
+
+import entities.card.NumberCard;
+
+public class GetCardOutputData {
+
+    final NumberCard numberCard;
+
+    public GetCardOutputData(NumberCard numberCard) {
+        this.numberCard = numberCard;
+    }
+
+    public NumberCard getNumberCard() {return this.numberCard;}
+
+
 }
