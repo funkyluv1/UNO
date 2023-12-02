7f339a75eb92cf8893d5bc9cd198f415982b8ed5
bluenut15 <bluenut15@gmail.com>
2023-12-01 21:30:26 -0500

    slight change to get card use case

--- src/use_case/GetCard/GetCardInteractor.java
+++ src/use_case/GetCard/GetCardInteractor.java
@@ -13,17 +13,14 @@ public class GetCardInteractor implements GetCardInputDataBoundary{
     public void execute(GetCardInputData player) {
         int randomNum = ThreadLocalRandom.current().nextInt(1,  10 + 1);
         int randomcol = ThreadLocalRandom.current().nextInt(1, 5);
-        String randomColor;
+        String randomColor = "";
 
         switch (randomcol) {
-            case 1 -> randomColor = "RED";
-            case 2 -> randomColor = "GREEN";
-            case 3 -> randomColor = "BLUE";
-            case 4 -> randomColor = "YELLOW";
+            case 1 -> randomColor = "R";
+            case 2 -> randomColor = "G";
+            case 3 -> randomColor = "B";
+            case 4 -> randomColor = "Y";
         }
-
-        // delete this later
-        randomColor = "RED";
 
         NumberCard card = (new NumberCardFactory(randomNum, randomColor)).createCard();
 
