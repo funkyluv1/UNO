2b6d0b048157b8b4789d3b1a1e6ff09a150eed9b
bluenut15 <bluenut15@gmail.com>
2023-12-04 15:18:25 -0500

    ai player restored

--- src/use_case/GetCard/GetCardInputData.java
+++ src/use_case/GetCard/GetCardInputData.java
@@ -2,12 +2,7 @@ package use_case.GetCard;
 
 public class GetCardInputData {
 
-    private final int index;
-
     public GetCardInputData(int index) {
-        this.index = index;
     }
-
-    public int getindex() {return this.index;}
 
 }
