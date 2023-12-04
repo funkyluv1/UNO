a33f0d3b5f7494a64d41f07de1d3d483f31f31e2
Hyunje Park <hyunje.park@mail.utoronto.ca>
2023-12-03 19:42:54 -0500

    added a new test case for some of the interactors

---
+++ test/use_case/SelectCard/SelectCardInteractorTest.java
@@ -0,0 +1,42 @@
+package use_case.SelectCard;
+
+import entities.Game;
+import entities.card.NumberCard;
+
+import java.util.ArrayList;
+
+public class SelectCardInteractorTest {
+
+    private Game game = new Game();
+
+
+
+    public void testSelectACard() {
+        ArrayList<NumberCard> numberCards = new ArrayList<>();
+        NumberCard num1 = new NumberCard(1, "G");
+        NumberCard num2 = new NumberCard(1, "R");
+        numberCards.add(num1);
+        numberCards.add(num2);
+
+    }
+
+    public void testSelectLastCard() {
+        ArrayList<NumberCard> numberCards = new ArrayList<>();
+        NumberCard num1 = new NumberCard(1, "G");
+        NumberCard num2 = new NumberCard(1, "R");
+        numberCards.add(num1);
+        numberCards.add(num2);
+
+    }
+
+
+    public void testSelectAfterUndo() {
+        ArrayList<NumberCard> numberCards = new ArrayList<>();
+        NumberCard num1 = new NumberCard(1, "G");
+        NumberCard num2 = new NumberCard(1, "R");
+        numberCards.add(num1);
+        numberCards.add(num2);
+
+    }
+
+}
