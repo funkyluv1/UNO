c67d066257fc56d4ae7fe88b11cc8dd4e0484feb
GuoxiangZ <106981818+GuoxiangZ@users.noreply.github.com>
2023-12-05 15:31:50 -0500

    changes nextturn

--- src/use_case/NextTurn/NextTurnDataAccessInterface.java
+++ src/use_case/NextTurn/NextTurnDataAccessInterface.java
@@ -1,1 +1,13 @@
+package use_case.NextTurn;
+import entities.card.FunctionalCard;
+import entities.card.NumberCard;
+import entities.player.Player;
 
+import java.util.ArrayList;
+
+
+public interface NextTurnDataAccessInterface {
+    Player getPlayer(int player_index);
+    void play_Card_and_update_DAO(String currPlayer, NumberCard selectedNumCard, ArrayList<FunctionalCard> selectedFunCard);
+
+}
--- src/use_case/NextTurn/NextTurnInteractor.java
+++ src/use_case/NextTurn/NextTurnInteractor.java
@@ -1,1 +1,24 @@
+import use_case.NextTurn.NextTurnDataAccessInterface;
+import use_case.NextTurn.NextTurnInputDataBoundary;
+import use_case.NextTurn.NextTurnOutputDataBoundary;
+import use_case.PostTurn.PostTurnInteractor;
+import use_case.PreTurn.FindPlayableCardsInterface;
+import use_case.PreTurn.PreTurnInteractor;
 
+public class NextTurnInteractor implements NextTurnInputDataBoundary {
+    final NextTurnDataAccessInterface fileUserDataAccessObject;
+    final NextTurnOutputDataBoundary nextTurn_presenter;
+    final FindPlayableCardsInterface findPlayableCardsInterface;
+    final PostTurnInteractor postTurnInteractor;
+    final PreTurnInteractor preTurnInteractor;
+
+    public NextTurnInteractor(NextTurnDataAccessInterface fileUserDataAccessObject, NextTurnOutputDataBoundary nextTurn_presenter,
+                              FindPlayableCardsInterface findPlayableCardsInterface, PostTurnInteractor postTurnInteractor,
+                              PreTurnInteractor preTurnInteractor) {
+        this.fileUserDataAccessObject = fileUserDataAccessObject;
+        this.nextTurn_presenter = nextTurn_presenter;
+        this.findPlayableCardsInterface = findPlayableCardsInterface;
+        this.postTurnInteractor = postTurnInteractor;
+        this.preTurnInteractor = preTurnInteractor;
+    }
+}
